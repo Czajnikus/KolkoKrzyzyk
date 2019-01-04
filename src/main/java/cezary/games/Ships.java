3a0ff68dca250f44d7384611ee031fbb2d61c868
Cezary Sturgulewski <czarstur@gmail.com>
2019-01-04 13:19:26 +0100

    Players can put their 1 ship on the board and shot each other

--- src/main/java/cezary/games/Ships.java
+++ src/main/java/cezary/games/Ships.java
@@ -1,5 +1,6 @@
 package cezary.games;
 
+import com.sun.javafx.css.SizeUnits;
 import java.util.Scanner;
 
 public class Ships {
@@ -29,98 +30,125 @@ public class Ships {
     //This will let players set up their ships on the maps
     public static void makingShips(String[][] shipsPlayer1, String[][] shipsPlayer2, String[][] shotPlayer1, String[][] shotPlayer2, Scanner scanner) {
         int turn = 1;
-        int p1s4 = 1;
-        int p1s3 = 2;
-        int p1s2 = 3;
-        int p1s1 = 4;
-        int p2s4 = 1;
-        int p2s3 = 2;
-        int p2s2 = 3;
-        int p2s1 = 4;
+        int p1s1 = 1;
+        int p2s1 = 1;
         System.out.println("Rozmieść swoje statki. Aby podać koordynaty, wpisz pierw literę, potem cyfrę.");
         boolean bothPlayers = true;
+        boolean gameplay = true;
+        boolean setup = true;
         while (bothPlayers) {
-            if (p1s4 == 1) {
-                System.out.println("Okręt 4 masztowy\n");
-                playerTurns(shipsPlayer1, shipsPlayer2, shotPlayer1, shotPlayer2, turn, scanner);
+            if (p1s1 != 0) {
+                System.out.println("Pierwszy\n");
+                playerTurns(shipsPlayer1, shipsPlayer2, shotPlayer1, shotPlayer2, turn, scanner, setup, gameplay);
+                p1s1 -= 1;
+                if (p1s1 == 0) {
+                    turn += 1;
+                }
+            } else if (p2s1 != 0) {
+                System.out.println("drugi\n");
+                playerTurns(shipsPlayer1, shipsPlayer2, shotPlayer1, shotPlayer2, turn, scanner, setup, gameplay);
+                p2s1 -= 1;
+                if (p2s1 == 0) {
+                    turn += 1;
+                }
+            } else {
+                bothPlayers = false;
             }
         }
-
-        
-
-        playerTurns(shipsPlayer1, shipsPlayer2, shotPlayer1, shotPlayer2, turn, scanner);
+        while (gameplay) {
+            setup = false;
+            playerTurns(shipsPlayer1, shipsPlayer2, shotPlayer1, shotPlayer2, turn, scanner, setup, gameplay);
+            turn += 1;
+        }
 
     }
 
     //Asks for coordinates
-    public static void coordinates(Scanner scanner, String[][] myTable, String[][] enemyTable) {
+    public static void coordinates(Scanner scanner, String[][] myTable, String[][] myShotsTable, String[][] enemyTable, Boolean setup, Boolean gameplay) {
         String position = scanner.next();
         int horizontal = 0;
         int vertical = 0;
         switch (position.toLowerCase().charAt(0)) {
             case 'a':
-                horizontal = 0;
+                vertical = 0;
                 break;
             case 'b':
-                horizontal = 1;
+                vertical = 1;
                 break;
             case 'c':
-                horizontal = 2;
+                vertical = 2;
                 break;
             case 'd':
-                horizontal = 3;
+                vertical = 3;
                 break;
             case 'e':
-                horizontal = 4;
+                vertical = 4;
                 break;
             case 'f':
-                horizontal = 5;
+                vertical = 5;
                 break;
             case 'g':
-                horizontal = 6;
+                vertical = 6;
                 break;
             case 'h':
-                horizontal = 7;
+                vertical = 7;
                 break;
             case 'i':
-                horizontal = 8;
+                vertical = 8;
                 break;
             case 'j':
-                horizontal = 9;
+                vertical = 9;
                 break;
         }
+
         if (position.charAt(position.length() - 1) - '0' == 0) {
-            vertical = 9;
+            horizontal = 9;
         } else {
-            vertical = position.charAt(position.length() - 1) - '0' - 1;
+            horizontal = position.charAt(position.length() - 1) - '0' - 1;
 
         }
-        //tu chyba trzeba będzie zrobić badanie komórki + zachowanie
-        myTable[horizontal][vertical] = myShip ;
-        enemyTable[horizontal][vertical] = enemyShip ;
+        if (setup) {
+            if (myTable[horizontal][vertical].equals(".")) {
+                myTable[horizontal][vertical] = "0";
+            }
+        } else if (gameplay) {
+            System.out.println("test");
+            if (enemyTable[horizontal][vertical].equals("0")) {
+                enemyTable[horizontal][vertical] = "X";
+                myShotsTable[horizontal][vertical] = "X";
+                System.out.println("Trafiony!");
+            } else if (enemyTable[horizontal][vertical].equals(".")) {
+                enemyTable[horizontal][vertical] = "v";
+                myShotsTable[horizontal][vertical] = "v";
+                System.out.println("Pudło!");
+            }
+
+        }
     }
 
     //This changes variables for each player's turn
-    public static void playerTurns(String[][] shipsPlayer1, String[][] shipsPlayer2, String[][] shotPlayer1, String[][] shotPlayer2, Integer turn, Scanner scanner) {
+    public static void playerTurns(String[][] shipsPlayer1, String[][] shipsPlayer2, String[][] shotPlayer1, String[][] shotPlayer2, Integer turn, Scanner scanner, Boolean setup, Boolean gameplay) {
 
         if (turn % 2 == 1) {
             String player = "Gracz 1";
             String[][] myTable = shipsPlayer1;
-            String[][] enemyTable = shotPlayer1;
-            tablesGenerator(player, myTable, enemyTable);
-            coordinates(scanner, myTable, enemyTable);
+            String[][] myShotsTable = shotPlayer1;
+            String[][] enemyTable = shipsPlayer2;
+            tablesGenerator(player, myTable, myShotsTable);
+            coordinates(scanner, myTable, myShotsTable, enemyTable, setup, gameplay);
         } else if (turn % 2 == 0) {
             String player = "Gracz 2";
             String[][] myTable = shipsPlayer2;
-            String[][] enemyTable = shotPlayer2;
-            tablesGenerator(player, myTable, enemyTable);
-            coordinates(scanner, myTable, enemyTable);
+            String[][] myShotsTable = shotPlayer2;
+            String[][] enemyTable = shipsPlayer1;
+            tablesGenerator(player, myTable, myShotsTable);
+            coordinates(scanner, myTable, myShotsTable, enemyTable, setup, gameplay);
         }
 
     }
 
     //Prints out both maps for the active player, the one with his shots and with his ships
-    public static void tablesGenerator(String player, String[][] myTable, String[][] enemyTable) {
+    public static void tablesGenerator(String player, String[][] myTable, String[][] myShotsTable) {
         System.out.println(player);
         System.out.println("XX|A|B|C|D|E|F|G|H|I|J   |   XX|A|B|C|D|E|F|G|H|I|J");
         String q = "|";
@@ -132,8 +160,8 @@ public class Ships {
                 if (b == 9) {
                     System.out.print("   |   ");
                     System.out.print(position);
-                    for (int c = 0; c < enemyTable.length; c++) {
-                        System.out.print(q + enemyTable[a][b]);
+                    for (int c = 0; c < myShotsTable[a].length; c++) {
+                        System.out.print(q + myShotsTable[a][c]);
                         if (c == 9) {
                             System.out.println();
                         }
